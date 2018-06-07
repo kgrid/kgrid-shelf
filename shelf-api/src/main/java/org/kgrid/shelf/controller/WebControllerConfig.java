3dcc03b08b59bb040bf0e2559a7dc0eb80c10aff
Peter Boisvert <pboisver@umich.edu>
2018-06-07 15:02:26 -0400

    add handling for '?format=zip' to allow simple download links

--- shelf-api/src/main/java/org/kgrid/shelf/controller/WebControllerConfig.java
+++ shelf-api/src/main/java/org/kgrid/shelf/controller/WebControllerConfig.java
@@ -1,6 +1,8 @@
 package org.kgrid.shelf.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,7 +11,18 @@ public class WebControllerConfig extends WebMvcConfigurerAdapter {
 
   @Override
   public void configurePathMatch(PathMatchConfigurer configurer) {
+    // don't match trailing version digit as file suffix, 'v0.0.1' =/= 'v0.0' w/ suffix '1'
     configurer.setUseSuffixPatternMatch(false);
   }
+
+  @Override
+  public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+    // adds handling for ?format=zip
+    configurer
+        .favorParameter(true)
+        .mediaType("zip", MediaType.parseMediaType("application/zip"))
+    ;
+  }
+
 
 }
