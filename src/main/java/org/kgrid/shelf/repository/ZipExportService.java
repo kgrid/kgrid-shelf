0d19439d71b87fae58efd2e47cf4cc4b2b8dfe83
Greg Farris <farrisg@umich.edus>
2018-11-21 13:56:10 -0500

    [#161975771] add initial parts of export service, test just for file system, not wired to controller or repp

---
+++ src/main/java/org/kgrid/shelf/repository/ZipExportService.java
@@ -0,0 +1,73 @@
+package org.kgrid.shelf.repository;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.ByteArrayOutputStream;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.kgrid.shelf.ShelfException;
+import org.kgrid.shelf.domain.ArkId;
+import org.zeroturnaround.zip.ByteSource;
+import org.zeroturnaround.zip.ZipEntrySource;
+import org.zeroturnaround.zip.ZipUtil;
+
+public class ZipExportService {
+
+  public static final String IMPLEMENTATIONS_TERM = "hasImplementation";
+  public static final String SERVICE_SPEC_TERM = "hasServiceSpecification";
+  public static final String DEPLOYMENT_SPEC_TERM = "hasDeploymentSpecification";
+  public static final String PAYLOAD_TERM = "hasPayload";
+  public static final String METADATA_FILENAME = "metadata.json";
+
+  public ByteArrayOutputStream exportCompoundDigitalObject(ArkId arkId,
+      CompoundDigitalObjectStore cdoStore) throws ShelfException {
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    List<ZipEntrySource> entries = new ArrayList();
+
+    //Get KO and add to export zip entries
+    ObjectNode koMetaData = cdoStore.getMetadata(arkId.getAsSimpleArk());
+    entries.add(new ByteSource(Paths.get(
+        arkId.getAsSimpleArk(), METADATA_FILENAME).toString(),
+        koMetaData.toString().getBytes()));
+
+    //Get KO Implementations
+    JsonNode implementations = koMetaData.findPath(IMPLEMENTATIONS_TERM);
+
+    implementations.forEach( jsonNode ->{
+
+      //Get and add KO Implementation metadat export zip entries
+      JsonNode implementationNode = cdoStore.getMetadata(
+          Paths.get(jsonNode.asText()).toString());
+      entries.add(new ByteSource(Paths.get(jsonNode.asText() , METADATA_FILENAME).toString(),
+          implementationNode.toString().getBytes()));
+
+      //Add Implementation binary files to export zip entries
+      List<String> binaryNodes = new ArrayList<>();
+      if (implementationNode.has(DEPLOYMENT_SPEC_TERM)) {
+        binaryNodes.add(implementationNode.findValue(DEPLOYMENT_SPEC_TERM).asText());
+      }
+      if (implementationNode.has(PAYLOAD_TERM)) {
+        binaryNodes.add(implementationNode.findValue(PAYLOAD_TERM).asText());
+      }
+      if (implementationNode.has(SERVICE_SPEC_TERM)) {
+          binaryNodes.add(implementationNode.findValue(SERVICE_SPEC_TERM).asText());
+      }
+      binaryNodes.forEach( (binaryPath) -> {
+        byte[] bytes = cdoStore.getBinary(
+            Paths.get( arkId.getAsSimpleArk(), binaryPath).toString());
+        entries.add(new ByteSource(
+            Paths.get( arkId.getAsSimpleArk(), binaryPath).toString(), bytes));
+      });
+    });
+
+    //Package it all up
+    ZipUtil.pack(entries.toArray(new ZipEntrySource[entries.size()]), outputStream);
+
+    return outputStream;
+  }
+
+
+}
---
+++ src/test/java/org/kgrid/shelf/repository/ZipExportServiceTest.java
@@ -0,0 +1,124 @@
+package org.kgrid.shelf.repository;
+
+import static java.nio.file.FileVisitOption.FOLLOW_LINKS;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.kgrid.shelf.domain.ArkId;
+import org.zeroturnaround.zip.ZipUtil;
+
+public class ZipExportServiceTest {
+
+  @ClassRule
+  public static TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private FilesystemCDOStore compoundDigitalObjectStore;
+
+  @Before
+  public void setUp() throws Exception {
+
+    String connectionURL = "filesystem:" + temporaryFolder.getRoot().toURI();
+    compoundDigitalObjectStore = new FilesystemCDOStore(connectionURL);
+    Path shelf = Paths.get(compoundDigitalObjectStore.getAbsoluteLocation(null));
+
+    Path helloWorld = Paths.get("src/test/resources/shelf/hello-world");
+    Path emptyWorld = Paths.get("src/test/resources/shelf/empty-world");
+    FileUtils.copyDirectory(helloWorld.toFile(),
+        Paths.get(shelf.toString(),"hello-world").toFile());
+    FileUtils.copyDirectory(emptyWorld.toFile(),
+        Paths.get(shelf.toString(),"empty-world").toFile());
+
+    temporaryFolder.newFolder("export");
+
+  }
+
+
+  @Test
+  public void exportCompoundDigitalObject() throws IOException {
+
+    ZipExportService zipExportService = new ZipExportService();
+
+    ByteArrayOutputStream outputStream = zipExportService.exportCompoundDigitalObject(
+        new ArkId("hello", "world"), compoundDigitalObjectStore);
+
+    writeZip(outputStream);
+
+    List<Path> filesPaths;
+    filesPaths = Files.walk(Paths.get(
+        temporaryFolder.getRoot().toPath().toString(),"export","hello-world"),  2, FOLLOW_LINKS)
+        .filter(Files::isRegularFile)
+        .map(Path::toAbsolutePath)
+        .collect(Collectors.toList());
+
+    filesPaths.forEach(file ->{
+      System.out.println(file.toAbsolutePath().toString());
+    });
+
+    assertEquals(9,filesPaths.size());
+
+  }
+
+  @Test
+  public void exportNoImplentationCompoundDigitalObject() throws IOException {
+
+    ZipExportService zipExportService = new ZipExportService();
+
+    ByteArrayOutputStream outputStream = zipExportService.exportCompoundDigitalObject(
+        new ArkId("empty", "world"), compoundDigitalObjectStore);
+
+    writeZip(outputStream);
+
+    List<Path> filesPaths;
+    filesPaths = Files.walk(Paths.get(
+        temporaryFolder.getRoot().toPath().toString(),"export","empty-world"),  2, FOLLOW_LINKS)
+        .filter(Files::isRegularFile)
+        .map(Path::toAbsolutePath)
+        .collect(Collectors.toList());
+
+    filesPaths.forEach(file ->{
+      System.out.println(file.toAbsolutePath().toString());
+    });
+
+    assertEquals(1,filesPaths.size());
+
+  }
+
+  protected void writeZip(ByteArrayOutputStream zipOutputStream) {
+
+    try {
+
+      Path path = Paths.get(temporaryFolder.getRoot().getAbsolutePath(), "export.zip");
+      OutputStream outputStream = new FileOutputStream(path.toString());
+      try {
+        zipOutputStream.writeTo(outputStream);
+      } finally {
+        outputStream.close();
+
+        ZipUtil.unpack(
+            Paths.get(temporaryFolder.getRoot().getAbsolutePath(), "export.zip").toFile(),
+            Paths.get(temporaryFolder.getRoot().getAbsolutePath(),"export").toFile());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  @After
+  public void cleanUp() throws IOException {
+
+    FileUtils.deleteDirectory( Paths.get(temporaryFolder.getRoot().getAbsolutePath(),"export").toFile());
+
+  }
+}
