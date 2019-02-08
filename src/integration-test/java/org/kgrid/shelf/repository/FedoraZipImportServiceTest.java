af075f40225a6cbe318657e572c36bdf64adeaca
gregfarris <farrisg@umich.edu>
2019-02-08 11:30:00 -0500

    [#162913826] Added fcrepo test import validation and transaction

--- src/integration-test/java/org/kgrid/shelf/repository/FedoraZipImportServiceTest.java
+++ src/integration-test/java/org/kgrid/shelf/repository/FedoraZipImportServiceTest.java
@@ -1,11 +1,15 @@
 package org.kgrid.shelf.repository;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -88,6 +92,30 @@ public class FedoraZipImportServiceTest {
 
   }
 
+  @Test
+  public void testBadKOMetaData() throws IOException {
 
+    InputStream zipStream = FedoraZipImportServiceTest.class
+        .getResourceAsStream("/fixtures/bad-kometadata.zip");
+
+    try{
+
+      service.findArkIdImportKO(zipStream, compoundDigitalObjectStore);
+
+      fail("should throw exception");
+
+    } catch (ShelfException se){
+
+      try {
+        ObjectNode metadata = compoundDigitalObjectStore.getMetadata(
+            Paths.get(new ArkId("hello", "world").getDashArk()).toString());
+        assertTrue("Should throw exception", false);
+      } catch (ShelfException e){
+        assertTrue("Should not find  hello world because not defined in meatadata, not found will throw exception", true);
+
+      }
+    }
+
+  }
 
 }
